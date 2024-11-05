e0b671ff76d30ce15e0785759565620609920440
Matija Zarić <76108670+mzaric13@users.noreply.github.com>
2024-11-05 09:30:07 +0100

    asd

---
+++ check-schema-second.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+until psql -h "second-instance-db" -U "postgres" -d "planet9" -c '\dn' | grep -q "planet9"; do
+  echo "Waiting for schema 'planet9' to be created..."
+  sleep 2
+done
+echo "Schema 'planet9' is ready!"
