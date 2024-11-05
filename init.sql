ff289431074d93ea43b15f14492e4f78a8e4bdc0
Matija Zarić <76108670+mzaric13@users.noreply.github.com>
2024-11-05 11:42:45 +0100

    test

--- init.sql
+++ init.sql
@@ -1,4 +1,45 @@
--- Function to create database if it does not exist
+-- -- Function to create database if it does not exist
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'first-database') THEN
+--         EXECUTE 'CREATE DATABASE "first-database"';
+--     END IF;
+-- END $$;
+
+-- -- Connect to the first-database
+-- \connect "first-database";
+
+-- -- Create the planet9 schema if it does not exist
+-- CREATE SCHEMA IF NOT EXISTS planet9;
+
+-- -- Function to create second database if it does not exist
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'second-database') THEN
+--         EXECUTE 'CREATE DATABASE "second-database"';
+--     END IF;
+-- END $$;
+
+-- -- Connect to the second-database
+-- \connect "second-database";
+
+-- -- Create the planet9 schema if it does not exist
+-- CREATE SCHEMA IF NOT EXISTS planet9;
+
+-- -- Function to create third database if it does not exist
+-- DO $$
+-- BEGIN
+--     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'third-database') THEN
+--         EXECUTE 'CREATE DATABASE "third-database"';
+--     END IF;
+-- END $$;
+
+-- -- Connect to the third-database
+-- \connect "third-database";
+
+-- -- Create the planet9 schema if it does not exist
+-- CREATE SCHEMA IF NOT EXISTS planet9;
+
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'first-database') THEN
@@ -6,13 +47,11 @@ BEGIN
     END IF;
 END $$;
 
--- Connect to the first-database
+-- Create the planet9 schema in first-database if it does not exist
 \connect "first-database";
-
--- Create the planet9 schema if it does not exist
 CREATE SCHEMA IF NOT EXISTS planet9;
 
--- Function to create second database if it does not exist
+-- Create the second-database if it does not exist
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'second-database') THEN
@@ -20,13 +59,11 @@ BEGIN
     END IF;
 END $$;
 
--- Connect to the second-database
+-- Create the planet9 schema in second-database if it does not exist
 \connect "second-database";
-
--- Create the planet9 schema if it does not exist
 CREATE SCHEMA IF NOT EXISTS planet9;
 
--- Function to create third database if it does not exist
+-- Create the third-database if it does not exist
 DO $$
 BEGIN
     IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'third-database') THEN
@@ -34,8 +71,6 @@ BEGIN
     END IF;
 END $$;
 
--- Connect to the third-database
+-- Create the planet9 schema in third-database if it does not exist
 \connect "third-database";
-
--- Create the planet9 schema if it does not exist
 CREATE SCHEMA IF NOT EXISTS planet9;
