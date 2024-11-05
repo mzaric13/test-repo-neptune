4a64b9ac39cf0d74a511ec5745d4e7712650b72b
Matija Zarić <76108670+mzaric13@users.noreply.github.com>
2024-11-05 11:52:19 +0100

    test

--- create_databases.sh
+++ create_databases.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-# Create the databases if they do not exist
-psql -U postgres -d postgres -c "SELECT 'CREATE DATABASE \"first-database\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'first-database') \gexec"
-psql -U postgres -d postgres -c "SELECT 'CREATE DATABASE \"second-database\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'second-database') \gexec"
-psql -U postgres -d postgres -c "SELECT 'CREATE DATABASE \"third-database\"' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'third-database') \gexec"
+# Function to create a database if it does not exist
+create_database() {
+    db_name="$1"
+    psql -U postgres -d postgres -tc "SELECT 1 FROM pg_database WHERE datname = '$db_name'" | grep -q 1 || psql -U postgres -d postgres -c "CREATE DATABASE \"$db_name\""
+}
+
+# Create databases
+create_database "first-database"
+create_database "second-database"
+create_database "third-database"
