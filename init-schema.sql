a931d98f9c9047f33ef382ccd90da1d33a2890cd
Matija Zarić <76108670+mzaric13@users.noreply.github.com>
2024-11-05 08:17:51 +0100

    asd

--- init-schema.sql
+++ init-schema.sql
@@ -1,1 +1,6 @@
-CREATE SCHEMA IF NOT EXISTS "planet9";
+DO $$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_namespace WHERE nspname = 'planet9') THEN
+      CREATE SCHEMA "planet9";
+   END IF;
+END $$;
